89e16b6e6f59a9d5bc70313f0a251dded8193027
Victor França <victorfclima@gmail.com>
2020-06-15 15:59:58 -0300

    Update: Created new routes (Post, Put and Delete)

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,10 +3,20 @@ const express = require('express');
 const app = express();
 const PORT = 3333;
 
-app.get('/', (request, response) => {
-	return response.json({
-		message: 'Hello World',
-	});
+app.get('/projects', (request, response) => {
+	return response.json(['Project 1', 'Project 2', 'Project 3']);
+});
+
+app.post('/projects', (request, response) => {
+	return response.json(['Project 1', 'Project 2', 'Project 3']);
+});
+
+app.put('/projects/:id', (request, response) => {
+	return response.json(['Project 4', 'Project 5', 'Project 6']);
+});
+
+app.delete('/projects/:id', (request, response) => {
+	return response.json(['Project 5', 'Project 6']);
 });
 
 app.listen(PORT, () => {
