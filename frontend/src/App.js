01a40216e2ba4fa0e9d855465189af26be2b0284
Victor França <victorfclima@gmail.com>
2020-06-16 19:33:18 -0300

    New: Added React Front-end

---
+++ frontend/src/App.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+
+import './App.css';
+
+import Header from './components/Header';
+
+function App() {
+	const [projects, setProjects] = useState([]);
+
+	useEffect(() => {
+		api.get('projects').then(response => {
+			setProjects(response.data);
+		});
+	}, []);
+
+	async function handleAddProject() {
+		const response = await api.post('projects', {
+			title: `Novo projeto ${Date.now()}`,
+			owner: 'Victor França C. de Lima',
+		});
+
+		const project = response.data;
+		setProjects([...projects, project]);
+	}
+
+	return (
+		<>
+			<Header title='Projects'></Header>
+			<ul>
+				{projects.map(project => {
+					return <li key={project.id}>{project.title}</li>;
+				})}
+			</ul>
+			<button type='button' onClick={handleAddProject}>
+				Adicionar projeto
+			</button>
+		</>
+	);
+}
+
+export default App;
