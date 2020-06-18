7d5754fd99768e46f6dbadbc7083ff93f6c9036a
Victor França <victorfclima@gmail.com>
2020-06-18 15:38:20 -0300

    New: Added Mobile Front-end React-Native

---
+++ mobile/src/index.js
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from 'react';
+import {
+	SafeAreaView,
+	Text,
+	StyleSheet,
+	StatusBar,
+	FlatList,
+	TouchableOpacity,
+	View,
+} from 'react-native';
+
+import api from './services/api';
+
+export default function App() {
+	const [projects, setProjects] = useState([]);
+
+	useEffect(() => {
+		api.get('projects').then(response => {
+			setProjects(response.data);
+		});
+	}, []);
+
+	async function handleAddProject() {
+		const response = await api.post('projects', {
+			title: `Project : #${Date.now()} added`,
+			owner: 'Victor França',
+		});
+
+		const newProject = response.data;
+
+		setProjects([...projects, newProject]);
+	}
+
+	async function handleRemoveProject(id) {
+		await api.delete(`projects/${id}`);
+
+		const otherProjects = projects.filter(projects => projects.id !== id);
+		setProjects(otherProjects);
+	}
+
+	return (
+		<>
+			<StatusBar barStyle='light-content' backgroundColor='#7159c1' />
+			<SafeAreaView style={styles.container}>
+				<FlatList
+					data={projects}
+					keyExtractor={project => project.id}
+					renderItem={({ item: project }) => {
+						return (
+							<View style={styles.row}>
+								<Text style={styles.project}>{project.title}</Text>
+								<TouchableOpacity
+									activeOpacity={0.6}
+									style={styles.buttonDelete}
+									onPress={() => handleRemoveProject(project.id)}
+								>
+									<Text style={styles.buttonTextDelete}>X</Text>
+								</TouchableOpacity>
+							</View>
+						);
+					}}
+				/>
+				<TouchableOpacity
+					activeOpacity={0.6}
+					style={styles.button}
+					onPress={handleAddProject}
+				>
+					<Text style={styles.buttonText}>ADICIONAR PROJETO</Text>
+				</TouchableOpacity>
+			</SafeAreaView>
+		</>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: '#7159c1',
+		flex: 1,
+	},
+
+	title: {
+		color: 'white',
+		fontSize: 20,
+		fontWeight: 'bold',
+	},
+
+	project: {
+		color: '#FFF',
+		fontSize: 20,
+	},
+
+	button: {
+		backgroundColor: '#FFF',
+		margin: 20,
+		height: 50,
+		borderRadius: 4,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+
+	buttonText: {
+		fontSize: 16,
+		fontWeight: 'bold',
+		justifyContent: 'center',
+	},
+
+	buttonDelete: {
+		backgroundColor: 'red',
+		margin: 20,
+		height: 20,
+		width: 20,
+		borderRadius: 4,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+
+	buttonTextDelete: {
+		color: 'white',
+		fontSize: 16,
+		fontWeight: 'bold',
+		justifyContent: 'center',
+	},
+
+	row: {
+		flexDirection: 'row',
+		alignItems: 'center',
+	},
+});
