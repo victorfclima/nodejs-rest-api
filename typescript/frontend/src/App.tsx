c4bd2f79aac48a32415e9db300c500e487742411
Victor França <victorfclima@gmail.com>
2020-06-19 16:52:23 -0300

    Update: ADD,Delete functions included

--- typescript/frontend/src/App.tsx
+++ typescript/frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import api from './services/api';
+import './App.css';
 
 function App() {
 	interface projectData {
@@ -14,20 +15,66 @@ function App() {
 		api.get('projects').then(response => setProjects(response.data));
 	}, []);
 
+	async function handleAddProject() {
+		await api
+			.post('projects', {
+				title: `Project #${Date.now()}`,
+				owner: 'Victor França Cavalcanti de Lima',
+			})
+			.then(response => setProjects(response.data));
+	}
+
+	async function handleDeleteProject(id: string) {
+		await api.delete(`projects/${id}`);
+		const updateProjects = projects.filter(project => project.id !== id);
+		setProjects(updateProjects);
+	}
+
+	function handleDeleteAllProjects() {
+		if (window.confirm('Delete all projects?')) {
+			projects.map(project => {
+				return api.delete(`projects/${project.id}`);
+			});
+			const updateProjects = projects.filter(project => '');
+			setProjects(updateProjects);
+		}
+	}
 	return (
 		<>
+			<button type='button' className='addButton' onClick={handleAddProject}>
+				Add new project
+			</button>
+			<button
+				type='button'
+				className='addButton'
+				onClick={handleDeleteAllProjects}
+			>
+				Delete all projects
+			</button>
 			<ul>
 				{projects.map(project => {
 					return (
-						<li key={project.id}>
-							<p>ID: {project.id}</p>
-							<p>Title: {project.title}</p>
-							<p>Owner: {project.owner}</p>
+						<li key={project.id} className='projectContainer'>
+							<div className='projectHeader'>
+								<p className='projectTitle'>{project.title}</p>
+								<button
+									type='button'
+									className='deleteButton'
+									onClick={() => handleDeleteProject(project.id)}
+								>
+									X
+								</button>
+							</div>
+							<div className='projectOwner'>
+								<p className='projectOwner'>{project.owner}</p>
+							</div>
+							<div className='projectFooter'>
+								<p className='projectId'>#{project.id}</p>
+							</div>
 						</li>
 					);
 				})}
 			</ul>
-			<button type='button'>Adicionar projeto</button>
 		</>
 	);
 }
